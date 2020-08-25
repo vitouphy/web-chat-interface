1e961bd0a5a5618f65cb7abe358791d416aad4fd
Vitou <phy_vitou@live.com>
2020-08-26 05:17:33 +0900

    center div

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,15 @@
     <!-- Particle Background -->
     <vue-particles class="particle" color="#bc97ec"  linesColor="#ceb6ec"></vue-particles>
 
-    <div>
+    <div style="position: relative; height: 100vh;">
+    <div class="center-vertical">
       <div>
         <h1 style="margin-bottom: 0;">Sandy Chatbot</h1>
-        <h4 style="margin: 0;">Your Chatting Companion</h4>
+        <h3>Your Chatting Companion</h3>
         <br/>
       </div>
       <div><basic-vue-chat :title="'CHAT HERE'" :new-message="message" @newOwnMessage="onNewOwnMessage" /></div>
+    </div>
     </div>
 
   </div>
@@ -54,15 +56,6 @@ export default {
 
 <style>
 
-.row {
-  /* display: flex; */
-  display: inline-block;
-}
-
-.column {
-  flex: 50%;
-}
-
 .particle {
   position: absolute;
   top: 0;
@@ -83,7 +76,18 @@ export default {
 
 .basic-vue-chat .window {
   margin: 0 auto;
-  max-width: 350px;
+  width: 700px;
+  height: 500px;
+  /* max-width: 350px; */
+  /* height: 100%; */
+}
+
+.center-vertical {
+  position: relative;
+  top: 50%;
+  -webkit-transform: translateY(-60%);
+  -ms-transform: translateY(-60%);
+  transform: translateY(-60%);
 }
 
 .basic-vue-chat #window__messages__container {
@@ -102,6 +106,8 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+  /* position: relative; */
 }
+
 </style>
